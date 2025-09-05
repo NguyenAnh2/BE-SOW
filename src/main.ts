08817a2f264913a242363f08df7e9f55fc5da39b
NguyenAnh2 <anht9402@gmail.com>
2025-09-05 12:02:21 +0700

    add CORS

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,12 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000, https://fesow.vercel.app',
     credentials: true,
   });
 
   app.setGlobalPrefix('api/v1/locals');
-  
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
